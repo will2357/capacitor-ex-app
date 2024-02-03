5a502b3ce3aa6ed13573a91243fb1f12d11dc13e
will2357 <will@highducheck.org>
2024-02-02 22:08:50 -0500

    Allow for string or blob

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -9,7 +9,7 @@ const PHOTO_STORAGE = 'photos';
 
 export function usePhotoGallery() {
   const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
-    let base64Data: string;
+    let base64Data: string | Blob;
     // "hybrid" will detect Cordova or Capacitor;
     if (isPlatform('hybrid')) {
       const file = await Filesystem.readFile({
