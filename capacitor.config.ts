297afd65f4d9a9dde50a6a77fab700adadace698
will2357 <will@highducheck.org>
2024-02-04 13:00:28 -0500

    deploy

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,14 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'photo-gallery',
   webDir: 'dist',
+  plugins: {
+    LiveUpdates: {
+      appId: '03cb2983',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 3
+    }
+  },
   server: {
     androidScheme: 'https'
   }
